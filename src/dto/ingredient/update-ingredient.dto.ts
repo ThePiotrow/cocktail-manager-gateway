5a7839174f87181e5d322c0c0e3f083968fdf114
Maxime MARCHAND <mmarchand6@myges.fr>
2023-07-04 16:01:26 +0200

    Update DTO

--- src/dto/ingredient/update-ingredient.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateIngredientDto } from './create-ingredient.dto';
-import { IsUUID } from 'class-validator';
-
-export class UpdateIngredientDto extends PartialType(CreateIngredientDto) {
-  @IsUUID()
-  id: string;
-}
